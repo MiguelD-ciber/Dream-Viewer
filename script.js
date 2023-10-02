a01a01a22fddb35e96d21833e97f1c116a5d1a74
MiguelD-ciber <83778713+MiguelD-ciber@users.noreply.github.com>
2023-10-02 15:19:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function gerarImagem() {
+    const lugarDescrito = document.getElementById("lugar").value;
+    const imagemGerada = document.getElementById("imagemGerada");
+    
+    // Aqui você pode usar um serviço externo para gerar uma imagem com base no texto inserido
+    // ou exibir uma imagem predefinida.
+    // Por exemplo:
+    // imagemGerada.innerHTML = `<img src="gerar_imagem.php?texto=${lugarDescrito}" alt="Imagem gerada">`;
+    
+    // Lembre-se de que a geração de imagens artificiais avançadas requer recursos adicionais e não pode ser feita apenas com HTML, CSS e JavaScript básicos.
+}
